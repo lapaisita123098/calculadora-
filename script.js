c5047aede1d692158ad768a8eb056e060df41757
lapaisita123098 <lapaisita123098@gmail.com>
2026-06-15 18:29:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,10 @@
 let pantalla = document.querySelector(".texto1");
-let boton1 = document.querySelector(".uno");
-boton1.addEventListener("click", function(){
-    pantalla.textContent+="1"
-});
-let boton2 = document.querySelector(".dos");
-boton2.addEventListener("click", function(){
-    pantalla.textContent+="2"
-});
-let boton3 = document.querySelector(".tres");
-boton3.addEventListener("click", function(){
-    pantalla.textContent+="3"
-});
-let boton4 = document.querySelector(".cuatro");
-boton4.addEventListener("click", function(){
-    pantalla.textContent+="4"
-})
-let boton5 = document.querySelector(".cinco");
-boton5.addEventListener("click", function(){
-    pantalla.textContent+="5"
-});
-let boton6 = document.querySelector(".seis")
-boton6.addEventListener("click", function(){
-    pantalla.textContent+="6"
-});
-let boton7 = document.querySelector(".siete")
-boton7.addEventListener("click", function(){
-    pantalla.textContent+="7"
-});
-let boton8 = document.querySelector(".ocho")
-boton8.addEventListener("click", function(){
-    pantalla.textContent+="8"
-});
-let boton9 = document.querySelector(".nueve")
-boton9.addEventListener("click", function(){
-    pantalla.textContent+="9"
-});
-let boton0 = document.querySelector(".cero")
-boton0.addEventListener("click", function(){
-    pantalla.textContent+="0"
-});
 let botonB = document.querySelector(".borrar")
 botonB.addEventListener("click", function(){
-    pantalla.textContent="" 
+pantalla.textContent=pantalla.textContent.slice(0, pantalla.textContent.length - 1)
+if(pantalla.textContent.length <= 10){
+    pantalla.style.fontSize = "28px"
+}
 });
 let numero1;
 let numero2;
@@ -58,71 +21,146 @@ function multiplicar(a, b){
 function dividir(a, b){
     return a / b
 };
+function calcular(){
+    numero2 = Number(pantalla.textContent)
+
+    switch (operacion){
+    case "+":{
+        numero1 = sumar(numero1, numero2)
+    }
+    break
+    case "-":{
+            numero1 = restar(numero1, numero2)
+            }
+    break
+    case "*":{
+            numero1 = multiplicar(numero1, numero2)
+    }
+    break
+    case "/":{
+            numero1 = dividir(numero1, numero2)
+    }
+    break
+}
+
+}
 let botonsuma = document.querySelector(".suma")
 botonsuma.addEventListener("click", function(){
-    operacion = "+"
-    numero1 = Number(pantalla.textContent)
-    if (!isNaN(numero1)){
-        pantalla.textContent=""
+    if(operacion){
+        calcular()
     }
+    else{
+        numero1 = Number(pantalla.textContent)
+    }
+    operacion = "+"
+    pantalla.textContent=""
 });
 let botonresta = document.querySelector(".resta")
 botonresta.addEventListener("click", function(){
-    operacion = "-"
-    numero1 = Number(pantalla.textContent)
-    if (!isNaN(numero1)){
-        pantalla.textContent=""
+    if(operacion){
+        calcular()
     }
+    else{
+        numero1 = Number(pantalla.textContent)
+    }
+    operacion = "-"
+    pantalla.textContent=""
 });
 let botonmultiplicar = document.querySelector(".multiplicar")
 botonmultiplicar.addEventListener("click", function(){
-    operacion = "*"
-    numero1 = Number(pantalla.textContent)
-    if (!isNaN(numero1)){
-        pantalla.textContent=""
+    if(operacion){
+        calcular()
     }
+    else{
+        numero1 = Number(pantalla.textContent)
+    }
+    operacion = "*"
+    pantalla.textContent=""
 });
 let botondividir = document.querySelector(".dividir")
 botondividir.addEventListener("click", function(){
-    operacion = "/"
-    numero1 = Number(pantalla.textContent)
-    if (!isNaN(numero1)){
-        pantalla.textContent=""
+    if(operacion){
+        calcular()
     }
+    else{
+        numero1 = Number(pantalla.textContent)
+    }
+    operacion = "/"
+    pantalla.textContent=""
 });
 let botonigual = document.querySelector(".igual")
 botonigual.addEventListener("click", function(){
                 numero2 = Number(pantalla.textContent)
+                
+let numero3;
 
 
 switch (operacion){
     case "+":{
             if (!isNaN(numero2)){
-                let numero3 = sumar(numero1, numero2)
-                pantalla.textContent=numero3;
+                numero3 = sumar(numero1, numero2)
     }
     }
     break
     case "-":{
             if (!isNaN(numero2)){
-                let numero3 = restar(numero1, numero2)
-                pantalla.textContent=numero3;
+                numero3 = restar(numero1, numero2)
             }
     }
     break
     case "*":{
             if (!isNaN(numero2)){
-                let numero3 = multiplicar(numero1, numero2)
-                pantalla.textContent=numero3;
+                numero3 = multiplicar(numero1, numero2)
             }
     }
     break
     case "/":{
             if (!isNaN(numero2)){
-                let numero3 = dividir(numero1, numero2)
-                pantalla.textContent=numero3;
+                numero3 = dividir(numero1, numero2)
             }
     }
     break
 }
+pantalla.textContent = numero3;
+
+numero1 = numero3;
+operacion = false;
+
+if(numero3.toString().length >= 10){
+    pantalla.style.fontSize = "14px"
+}
+});
+
+let botones = document.querySelectorAll("button")
+for(let inicio = 0; inicio < botones.length; inicio++){
+    botones[inicio].addEventListener("click", function(){
+        if(!isNaN(botones[inicio].textContent)){
+        if(pantalla.textContent.length < 11){
+            pantalla.textContent += botones[inicio].textContent
+        }}
+    })
+};
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Backspace"){
+        botonB.click();
+    }
+    if(event.key === "Enter"){
+        botonigual.click();
+    }
+    if(event.key === "c"){
+        botonc.click();
+    }
+    for(let inicio2 = 0; inicio2 < botones.length; inicio2++){
+        if(botones[inicio2].textContent === event.key){
+            botones[inicio2].click();
+        }
+    }
+})  
+
+let botonc = document.querySelector(".borrartodo")
+botonc.addEventListener("click", function(){
+    operacion=false
+    pantalla.textContent=""
+    pantalla.style.fontSize="28px"
 });
